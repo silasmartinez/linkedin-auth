8a11f754d793ca5c0c907c70200195bd489f4f6c
silasmartinez <silasm@gmail.com>
2015-07-20 13:27:24 -0600

    Smaller profile object

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ passport.use(new LinkedInStrategy({
     // represent the logged-in user. In a typical application, you would want
     // to associate the LinkedIn account with a user record in your database,
     // and return that user instead.
-    return done(null, profile);
+    return done(null, {id: profile.id, displayName: profile.displayName});
   })
 }))
 
